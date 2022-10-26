ee2acf32efb352fc73df44098385ea88cd5dfe27
Robertson Arthur <robertsonakpan@gmail.com>
2022-10-26 01:01:59 +0100

    Add validation to reservation table

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :car
+
+  validates :city, :reservation_date, :car_id, :user_id presence: true
+  # validates :car_id, uniqueness: { scope: :user_id }
 end

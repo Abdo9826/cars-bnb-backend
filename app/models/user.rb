d4d0d226bc41f92dcf5dae46737aa504ee69333a
unknown <abodyalex1@gmail.com>
2022-10-26 15:59:31 +0300

    modify user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  has_many :cars, dependent: :destroy
+  #whats dependent: :destroy?
+  #it means that if the user is deleted, the cars will be deleted as well
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self

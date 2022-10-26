460d3cb16e2b0ff874773b9320bd7117ff91de3b
Robertson Arthur <robertsonakpan@gmail.com>
2022-10-26 01:06:12 +0100

    Fix linters

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,6 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :car
 
-  validates :city, :reservation_date, :car_id, :user_id presence: true
+  validates :city, :reservation_date, :car_id, :user_id, presence: true
   # validates :car_id, uniqueness: { scope: :user_id }
 end

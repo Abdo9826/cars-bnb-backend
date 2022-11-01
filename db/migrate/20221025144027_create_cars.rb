46915802c425d8baaf70bc6328e12f6d64e7b66b
shahnawaza75 <shahnawaza75@gmail.com>
2022-11-01 18:48:47 +0100

    Remove year and reference for user in Cars table migration file.

--- db/migrate/20221025144027_create_cars.rb
+++ db/migrate/20221025144027_create_cars.rb
@@ -3,9 +3,7 @@ class CreateCars < ActiveRecord::Migration[7.0]
     create_table :cars do |t|
       t.string :model
       t.string :brand
-      t.string :year
       t.datetime :year_released
-      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end

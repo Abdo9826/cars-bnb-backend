48711ec510cb7942a7fd0f796ba6b7fbffca8256
shahnawaza75 <shahnawaza75@gmail.com>
2022-11-01 18:49:30 +0100

    Remove unneccessary column user_name in reservation table

--- db/migrate/20221025203058_create_reservations.rb
+++ db/migrate/20221025203058_create_reservations.rb
@@ -2,7 +2,6 @@ class CreateReservations < ActiveRecord::Migration[7.0]
   def change
     create_table :reservations do |t|
       t.date :reservation_date
-      t.string :user_name
       t.string :city
       t.references :user, null: false, foreign_key: true
       t.references :car, null: false, foreign_key: true
--- db/migrate/20221028172539_add_image_to_car.rb
+++ db/migrate/20221028172539_add_image_to_car.rb
@@ -1,5 +1,5 @@
 class AddImageToCar < ActiveRecord::Migration[7.0]
   def change
-    add_column :cars, :featured_image, :string, default: false
+    add_column :cars, :featured_image, :string
   end
 end

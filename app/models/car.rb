86b98726de5e95ba782fea218e999fe2630dba89
bobb-rob <robertsonakpan@gmail.com>
2022-11-05 22:15:45 +0100

    Add depenednt destroy to Cars model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,5 @@
 class Car < ApplicationRecord
-  has_many :reservation, foreign_key: :car_id
+  has_many :reservation, foreign_key: :car_id, dependent: :destroy
 
   validates :model, :brand, :year_released, :featured_image, presence: true
 end
